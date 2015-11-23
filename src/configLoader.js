590ef539df5b4483b42cbd5d8e512b4851cfa54e
Ineentho <hk.henrik@gmail.com>
2015-11-23 22:00:04 +0100

    Config for sway_host

--- src/configLoader.js
+++ src/configLoader.js
@@ -9,6 +9,7 @@ function init(cb) {
     nconf.set('port', '3000');
     nconf.set('mongo', 'mongodb://localhost/gigavoid-sway');
     nconf.set('auth_url', 'http://accounts-api.gigavoid.com/verify');
+    nconf.set('sway_host', 'sway');
 
     nconf.save(function (err) {
         if (err) {
